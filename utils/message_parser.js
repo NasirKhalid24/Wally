774ed8688a285d42c93b55bc8435aee3219a8690
adib1996 <adib1996@live.com>
2020-09-13 23:27:34 +0400

    :sparkles: Adding date time and group message idetifier

--- utils/message_parser.js
+++ utils/message_parser.js
@@ -9,13 +9,22 @@ module.exports = message_parser = async(client, message) => {
         // 4. after checking from prefix get the 'sticker' keyword (switch statement so we can add other keywords in future)
         // 5. extract all images from message in case they send multiple
         // 6. send back all images as stickers
+        
+        if (message.body === '#sticker' && message.isGroupMsg == true) {
+            let unix_timestap = message.timestamp
+            var date = new Date(unix_timestap * 1000);
+            var formattedTime = date;
 
-        if (message.body === 'Hi') {
+            console.log({"Message content": `${message.body}`,"Time send":`${formattedTime}`});
+
+
             client.sendText(message.from, '👋👋🏼 Hello!');
         }
 
     } catch(err){
         console.log("ERROR IN MESSAGE PARSER = ", err);
     }
+
+
 
 }

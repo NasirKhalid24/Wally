e4bf11d938f142277f1964c8d02bd370a2f4807e
NasirK24 <nasirmkhalid24@gmail.com>
2020-09-13 19:29:56 +0400

    ❌ Added error handling

--- index.js
+++ index.js
@@ -1,35 +1,61 @@
 const wa = require('@open-wa/wa-automate');
 
-const message_parser = require('./utils/message_parser');
+// const message_parser = require('./utils/message_parser');
 const MESSAGE_PREFIX = '!';
 
-wa.create().then(client => start(client));
+async function launch(){
+    try{
+        const client = await wa.create({
+            //sets restartOnCrash to the above `start` function
+            restartOnCrash: start,
+
+            //log all browser console output
+            logConsole: true,
+
+            //or just browser errors
+            logConsoleErrors: true,
+
+            //kill the process if the browser crashes/is closed manually
+            killProcessOnBrowserClose: true
+        });
+        await start(client);
+    } catch(error){
+        console.log(error)
+    }
+}
 
 function start(client) {
 
-    // Handling any changes to the state of the bot 
-    client.onStateChanged(state=>{
-        console.log('STATE CHANGE = ', state)
-        if(state==="CONFLICT" || state==="UNLAUNCHED") client.forceRefocus();
-    
-        if(state==='UNPAIRED') console.log('Got logged out! Restart server and scan QR code!')
-      });
+    try{
+        // Handling any changes to the state of the bot 
+        client.onStateChanged(state=>{
+            console.log('STATE CHANGE = ', state)
+            if(state==="CONFLICT" || state==="UNLAUNCHED") client.forceRefocus();
+        
+            if(state==='UNPAIRED') console.log('Got logged out! Restart server and scan QR code!')
+        });
 
-    // Handling incoming messages
-    client.onMessage(message => {
-        if (message.body === 'Hi') {
-        client.sendText(message.from, '👋 Hello!');
-        }
-    });
+        // Handling incoming messages
+        client.onMessage(message => {
+            if (message.body === 'Hi') {
+            client.sendText(message.from, '👋 Hello!');
+            }
+        });
 
-    // Handling incoming Whatsapp calls
-    client.onIncomingCall(async call=>{
-        await client.sendText(call.peerJid._serialized, 'Sorry, I cannot accept calls 📵');
-    });
+        // Handling incoming Whatsapp calls
+        client.onIncomingCall(async call=>{
+            await client.sendText(call.peerJid._serialized, 'Sorry I cannot accept calls 📵');
+        });
 
-    // Handle being added to group
-    client.onAddedToGroup(({ groupMetadata: { id }, contact: { name } }) => {
-        client.sendText(id, 'Sorry I cannot be added to groups (as of now 😉)').then(() => client.leaveGroup(id))
-    })
+        // Handle being added to group
+        client.onAddedToGroup(({ groupMetadata: { id }, contact: { name } }) => {
+            client.sendText(id, 'Sorry I cannot be added to groups (as of now 😉)').then(() => client.leaveGroup(id))
+        })
+    }catch(error){
+        client.kill();
+        console.log("ERROR = ", error)
+    }
 
 }
+
+launch();

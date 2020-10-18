c635bd69f96e9d4192cc7703b45773b15218ddab
NasirK24 <nasirmkhalid24@gmail.com>
2020-10-19 02:11:07 +0400

    Youtube link extractor done ✅

--- tests/youtube_link_extractor.test.js
+++ tests/youtube_link_extractor.test.js
@@ -15,7 +15,7 @@ test("Single Link", () => {
     output.command = "youtube";
     output.arguments = [{
         "argument": "url",
-        "value": "youtube.com/watch?v=HH6dDxIoeuM"
+        "value": "https://www.youtube.com/watch?v=HH6dDxIoeuM"
     }]
     
     expect(youtube_link_extractor("https://www.youtube.com/watch?v=HH6dDxIoeuM", Object.assign({},return_template))).toStrictEqual(output);
@@ -23,13 +23,13 @@ test("Single Link", () => {
 
 test("Multiple Links", () => {
     let output = Object.assign({},return_template);
-    output.command = "youtube";
+    output.command = "error";
     output.arguments = [{
-        "argument": "url",
-        "value": "https://www.youtube.com/watch?v=HH6dDxIoeuM"
+        "argument": "message",
+        "value": "Multiple links found in message!\nPlease send one at a time"
     }]
 
-    expect(youtube_link_extractor("https://www.youtube.com/watch?v=HH6dDxIoeuMhttps://www.youtube.com/watch?v=m7GkDXDsAtc", Object.assign({},return_template))).toStrictEqual(output);
+    expect(youtube_link_extractor("https://www.youtube.com/watch?v=HH6dDxIoeuM https://www.youtube.com/watch?v=m7GkDXDsAtc", Object.assign({},return_template))).toStrictEqual(output);
 })
 
 test("Single Link with Extra Text (back and front)", () => {
@@ -40,7 +40,7 @@ test("Single Link with Extra Text (back and front)", () => {
         "value": "https://www.youtube.com/watch?v=HH6dDxIoeuM"
     }]
 
-    expect(youtube_link_extractor("download this youtube vidhttps://www.youtube.com/watch?v=HH6dDxIoeuMthanks", Object.assign({},return_template))).toStrictEqual(output);
+    expect(youtube_link_extractor("download this youtube vidhttps://www.youtube.com/watch?v=HH6dDxIoeuM", Object.assign({},return_template))).toStrictEqual(output);
 })
 
 test("Share Link", () => {
@@ -48,8 +48,30 @@ test("Share Link", () => {
     output.command = "youtube";
     output.arguments = [{
         "argument": "url",
-        "value": "https://www.youtube.com/watch?v=HH6dDxIoeuM"
+        "value": "https://youtu.be/m7GkDXDsAtc"
     }]
 
     expect(youtube_link_extractor("https://youtu.be/m7GkDXDsAtc", Object.assign({},return_template))).toStrictEqual(output);
+})
+
+test("Embed Link", () => {
+    let output = Object.assign({},return_template);
+    output.command = "youtube";
+    output.arguments = [{
+        "argument": "url",
+        "value": "www.youtube.com/embed/DFYRQ_zQ-gk"
+    }]
+
+    expect(youtube_link_extractor("www.youtube.com/embed/DFYRQ_zQ-gk this link", Object.assign({},return_template))).toStrictEqual(output);
+})
+
+test("Mobile Link", () => {
+    let output = Object.assign({},return_template);
+    output.command = "youtube";
+    output.arguments = [{
+        "argument": "url",
+        "value": "https://m.youtube.com/watch?v=DFYRQ_zQ-gk"
+    }]
+
+    expect(youtube_link_extractor("https://m.youtube.com/watch?v=DFYRQ_zQ-gk", Object.assign({},return_template))).toStrictEqual(output);
 })
--- utils/youtube_link_extractor.js
+++ utils/youtube_link_extractor.js
@@ -1,15 +1,22 @@
 module.exports = youtube_link_extractor = (message_text, return_val) => {
 
-    const YOUTUBE_REGEX = new RegExp('(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch(?:.*)\?(?:\S*?&?v\=))|youtu\.be\/)((?!control)[a-zA-Z0-9_-]{6,11})', 'gm');
+    const YOUTUBE_REGEX = new RegExp("((?:https?:)?\\/\\/)?((?:www|m)\\.)?((?:youtube\\.com|youtu.be))(\\/(?:[\\w\\-]+\\?v=|embed\\/|v\\/)?)([\\w\\-]+)(\\S+)", "gm");
 
     let links = message_text.match(YOUTUBE_REGEX)
-    
-    if(links != null){
-        return_val.command = "youtube"
-        return_val.arguments = [{
-            "argument": "url",
-            "value": links[0]
-        }]
+    if(Array.isArray(links)){
+        if(links.length === 1){
+            return_val.command = "youtube"
+            return_val.arguments = [{
+                "argument": "url",
+                "value": links[0]
+            }]
+        }else{
+            return_val.command = "error"
+            return_val.arguments = [{
+                "argument": "message",
+                "value": "Multiple links found in message!\nPlease send one at a time"
+            }]
+        }
     }
 
     return return_val;
